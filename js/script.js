0ac252f3729cc94f85d8f8bd3e4250b588274d02
pierredopchie <pierre.dopchie@hotmail.com>
2019-08-27 23:52:00 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-jQuery(document).ready(function() {
-	var contentHeight = jQuery(window).height();
-	var footerHeight = jQuery('.social-links').height();
-	var footerTop = jQuery('.social-links').position().top + footerHeight;
-	if (footerTop < contentHeight) {
-		jQuery('.social-links').css('margin-top', 10+ (contentHeight - footerTop) + 'px');
-	}
+// jQuery(document).ready(function() {
+// 	var contentHeight = jQuery(window).height();
+// 	var footerHeight = jQuery('.social-links').height();
+// 	var footerTop = jQuery('.social-links').position().top + footerHeight;
+// 	if (footerTop < contentHeight) {
+// 		jQuery('.social-links').css('margin-top', 10+ (contentHeight - footerTop) + 'px');
+// 	}
+// });
+
+
+
+
+$( document ).ready(function() {
+	jQuery(function($) {
+		var path = window.location.href;
+		$('li a').each(function() {
+			if (this.href === path) {
+				$(this).addClass('active');
+			}
+		});
+	});
 });
