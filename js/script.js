ce2aa97ce22864db040b3f484625caf252b3e42f
pierredopchie <pierre.dopchie@hotmail.com>
2019-08-29 20:25:22 +0200

    menu burger

--- js/script.js
+++ js/script.js
@@ -11,12 +11,5 @@
 
 
 $( document ).ready(function() {
-	jQuery(function($) {
-		var path = window.location.href;
-		$('li a').each(function() {
-			if (this.href === path) {
-				$(this).addClass('active');
-			}
-		});
-	});
+
 });
