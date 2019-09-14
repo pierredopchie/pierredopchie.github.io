8384d2b14b7585f42f36f8a56176df0768406f55
pierredopchie <43740960+pierredopchie@users.noreply.github.com>
2019-09-14 17:59:31 +0200

    14 sept

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-// jQuery(document).ready(function() {
-// 	var contentHeight = jQuery(window).height();
-// 	var footerHeight = jQuery('.social-links').height();
-// 	var footerTop = jQuery('.social-links').position().top + footerHeight;
-// 	if (footerTop < contentHeight) {
-// 		jQuery('.social-links').css('margin-top', 10+ (contentHeight - footerTop) + 'px');
-// 	}
-// });
+jQuery(document).ready(function() {
+	var contentHeight = jQuery(window).height();
+	var footerHeight = jQuery('.social-links').height();
+	var footerTop = jQuery('.social-links').position().top + footerHeight;
+	if (footerTop < contentHeight) {
+		jQuery('.social-links').css('margin-top', 10+ (contentHeight - footerTop) + 'px');
+	}
+});
 
 
 
 
 $( document ).ready(function() {
+
+    window.onscroll = function () {
+        scrollFunction()
+    };
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $(".nav__links").backgroundColor = 'rgba(0,0,0,1)';
+        } else {
+            $(".nav__links").backgroundColor = "transparent";
+        }
+    }
 
 });
